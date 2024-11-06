538ded75f7cb9e1bf168e70d0fb548fd13baa7ff
Matheus Marques de Andrade <2302994@SALAS.AULAS>
2024-11-06 09:30:57 -0300

    AP2 calculadora

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+def soma_2302994_2303258(som1,som2):
+    return som1 + som2
+
+def subtrair_2302994_2303258(sub1,sub2):
+    return sub1 - sub2
+
+def multiplicar_2302994_2303258(mul1,mul2):
+    return mul1 * mul2
+
+def dividir_2302994_2303258(div1,div2):
+    return div1 / div2
+
+
+soma_2302994_2303258(5,7)
+subtrair_2302994_2303258(10,5)
+multiplicar_2302994_2303258(5,9)
+dividir_2302994_2303258(10,2)
