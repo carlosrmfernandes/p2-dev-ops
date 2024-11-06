895daeb2dec5acfc53d0c765e4b098158abe5d19
JasminShadday <shaddayjasmim@gmail.com>
2024-11-06 10:06:11 -0300

    subindo arquivos

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def soma_2303142_2302304(n1,n2):
+    return n1+n2
+
+def subtrair_2303142_2302304(n1,n2):
+    return n1-n2
+    
+def multiplicar_2303142_2302304(n1,n2):
+    return n1*n2
+    
+def dividir_2303142_2302304(n1,n2):
+    return n1/n2
