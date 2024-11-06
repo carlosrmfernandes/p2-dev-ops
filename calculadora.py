f0d3feb70d083cedab3ed682f68858613b29ed58
Ygor Martins <ygormartins27ar@gmail.com>
2024-11-06 10:39:09 -0300

    Criando funções

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+def somar(numero1,numero2):
+    return numero1+numero2
+
+def subtrair(numero1, numero2):
+    return numero1 - numero2
+
+def dividir(numero1, numero2):
+    return numero1/numero2
+
+
+def multuplicar(numero1,numero2):
+    numero1*numero2
